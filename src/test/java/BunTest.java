ac87203ec1a07cd42e974f617e85b24f108c7a73
panchenkov-scr <89070652+panchenkov-scr@users.noreply.github.com>
2023-01-25 11:11:42 +0400

    Four commit - исправления и дополнения

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,32 +1,63 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class BunTest {
-  Bun bun;
 
-  private final String BUN = "black bun";
-  private final int BUN_PRICE = 100;
+  private final String name;
+  private final float bunPrice;
 
-  @Before
-  public void setUp(){
-    bun = new Bun(BUN, BUN_PRICE);
+  public BunTest(String name, float bunPrice) {
+    this.name = name;
+    this.bunPrice = bunPrice;
   }
 
+  @Parameterized.Parameters
+  public static Object[][] dataTest() {
+    return new Object[][]{
+            {"black bun", 100f},
+            {"", 100f},
+            {null, 100f},
+            {"£%$^", 100f},
+            {"dfhdshdshhjtjrtjjnrtghjdehjndshdfbdfbdfhfhdfhgrhrthj", 100f},
+            {"black bun", 0},
+            {"", 0},
+            {null, 0},
+            {"£%$^", 0},
+            {"dfhdshdshhjtjrtjjnrtghjdehjndshdfbdfbdfhfhdfhgrhrthj", 0},
+            {"black bun", Float.MAX_VALUE},
+            {"", Float.MAX_VALUE},
+            {null, Float.MAX_VALUE},
+            {"£%$^", Float.MAX_VALUE},
+            {"dfhdshdshhjtjrtjjnrtghjdehjndshdfbdfbdfhfhdfhgrhrthj", Float.MAX_VALUE},
+            {"black bun", -100f},
+            {"", -100f},
+            {null, -100f},
+            {"£%$^", -100f},
+            {"dfhdshdshhjtjrtjjnrtghjdehjndshdfbdfbdfhfhdfhgrhrthj", -100f},
+            {"black bun", 0.000000000001f},
+            {"", 0.000000000001f},
+            {null, 0.000000000001f},
+            {"£%$^", 0.000000000001f},
+            {"dfhdshdshhjtjrtjjnrtghjdehjndshdfbdfbdfhfhdfhgrhrthj", 0.000000000001f}
+    };
+  }
 
   @Test
-  public void getNameBunTest(){
-    String actualBun = "black bun";
-    String expectedBun = bun.getName();
-    Assert.assertEquals("Ожидается имя булки - black bun", expectedBun, actualBun);
+  public void getNameBunTest() {
+    Bun bun = new Bun(name, bunPrice);
+    assertEquals("Ожидается корректное имя булки", name, bun.getName());
   }
 
   @Test
   public void getPriceBunTest() {
-    float expectedPrice = 100;
-    float actualPrice = bun.getPrice();
-    Assert.assertEquals("Ожидается цена - 100", expectedPrice, actualPrice, 0);
+    Bun bun = new Bun(name, bunPrice);
+    Assert.assertEquals("Ожидается корректная цена", bunPrice, bun.getPrice(), 0);
   }
 
 }
